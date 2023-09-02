b9398467db55f8c892130857519660f73a3949f8
Kiana8181 <kianahashemi8@gmail.com>
2023-09-02 23:43:15 +0330

    add routes

---
+++ src/routing/routes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./HomePage";
+import UserListPage from "./UserListPage";
+import ContactPage from "./ContactPage";
+import UserDetailPage from "./UserDetailPage";
+import Layout from "./Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "users", element: <UserListPage /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+    ],
+  },
+  { path: "/contacts", element: <ContactPage /> },
+]);
+
+export default router;
